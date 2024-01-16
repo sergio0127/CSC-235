b5b17e2ac66dccd4d1d2bf296c7492814a16f866
sergio0127 <151782536+sergio0127@users.noreply.github.com>
2024-01-15 21:26:47 -0800

    Add files via upload

---
+++ CSC 235 Week 1.py
@@ -0,0 +1,43 @@
+# Week 1 assignment - Sergio Y Cano
+# For this assignment we need to creat a tutorial about a python feature
+# This code needs to have :
+# input & output
+# We need to have lots of code comments.
+
+# We need to provide an example about the print command.
+print('Week 1 python feature tutorial')
+print('In this tutorial we are going to master the print command!')
+
+
+# Create Two Variables
+# Gather user input
+ReadyYN = input('Are you ready? (Y/N)')
+
+
+
+
+# Display user input
+print('You answered: , ReadyYN')
+
+
+# print some more information about Python
+print('What do you think the print command is for? ')
+print(' 321 Ok! Times Up!')
+print('If you believe it was "print" you are correct!')
+print('----------------------------------------')
+print('Here is the syntax for the "print" command in Python')
+print('print("Text to display on the screen."')
+print('----------------------------------------')
+print('Here is how you would use the "print" command in Python to print out the phrase "I am skynet from the future"')
+print('I am skynet from the future')
+print('Pro tip: the print command is case sensitive, it must be all lowercase.')
+print('----------------------------------------')
+
+# Create the second variable
+rating = input(' Rate this turorial on a scale of 1 to 10 (With 10 being the best)')
+
+
+
+print('You thought this tutorial was a : ', rating)
+
+print('End of Tutorial... Have a nice day!')
